cccd9db92297e27ca87adc58827c40e1dba5a050
FTMahringer <168755023+FTMahringer@users.noreply.github.com>
2026-04-26 11:12:14 +0000

    chore: update registry [skip ci]

--- flakes/ft-nixlaunch/default.nix
+++ flakes/ft-nixlaunch/default.nix
@@ -12,8 +12,8 @@ in
     repo         = "github:FT-nixforge/ft-nixlaunch";
     provides     = [ "packages" "homeModules" "overlays" ];
     dependencies = [ "ft-nixpalette" ];
-    version      = "2.1.0";
-    versions     = [ "v2.1.0" "v2.0.0" "v1.0.0" "v0.1.0" ];
+    version      = "2.2.0";
+    versions     = [ "v2.2.0" "v2.1.0" "v2.0.0" "v1.0.0" "v0.1.0" ];
   };
 
   packages    = flake.packages.${system} or {};
